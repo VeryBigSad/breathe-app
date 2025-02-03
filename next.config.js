8cc66aabcac6fa18f09d64ea881ca74915a6c627
Mikhail Khromov <khromov05@gmail.com>
2025-02-03 04:12:39 +0300

    add github pages auto-deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/breathe-app' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
